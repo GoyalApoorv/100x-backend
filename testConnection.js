4cf077f5d314ca321edc38de743afe6c1c99981d
ApoorvGoyal <apoorvgoyal1212@gmail.com>
2024-04-03 17:47:21 +0530

    initial commit

---
+++ testConnection.js
@@ -0,0 +1,13 @@
+db = require('./models/index');
+
+(async() => {
+    try {
+        await db.sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+      finally {
+        await db.sequelize.close();
+      }
+})();
